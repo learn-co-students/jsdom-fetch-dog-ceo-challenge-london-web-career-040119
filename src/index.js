a909e005b6616633063d02b191985010519e259d
hlbtyne <harriet.ballantyne@gmail.com>
2019-05-15 18:02:27 +0100

    Challenge 2 + 3 passing

--- src/index.js
+++ src/index.js
@@ -13,10 +13,52 @@ function renderImage(image) {
     container.appendChild(imageWrapper)
 }
 
-  function renderImages (images) {
-    images.forEach(image => renderImage(image) )
+  function renderImages(images) {
+    images.forEach(image => renderImage(image))
   }
   
 getImages()
-    .then((images) => renderImages(images))
 
+
+    
+
+
+
+function getBreeds() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(obj => obj.message)
+    .then(breeds => renderBreeds(Object.keys(breeds)))
+}
+
+function renderBreed(breed) {
+    const list = document.querySelector('#dog-breeds')
+    const listItem = document.createElement('li')
+
+    listItem.innerHTML = `${breed}`
+    listItem.addEventListener('click', function() {
+        listItem.style.color = "purple"
+    })
+    list.appendChild(listItem)
+}
+
+function renderBreeds(breeds) {
+    // breeds = breeds
+    breeds.forEach(breed => renderBreed(breed))
+}
+
+getBreeds()
+
+
+
+// dropdown = document.querySelector('breed-dropdown').value
+
+// function listBreeds(dropdown){
+//     for (let i=0;i<breeds.length;i++){
+//       breeds[i].startsWith(dropdown)
+//       }
+//   }
+  
+//   let startsWithN = breeds.filter(listBreeds(dropdown))
+
+//   listBreeds(dropdown)

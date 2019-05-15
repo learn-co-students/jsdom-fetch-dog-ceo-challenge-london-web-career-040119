0f4e38e80a248e327190a5d45f966758f4dca076
hlbtyne <harriet.ballantyne@gmail.com>
2019-05-15 17:02:56 +0100

    First challenge completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+function getImages() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(obj => obj.message)
+    .then(images => renderImages(images))
+}
+
+function renderImage(image) {
+    const container = document.querySelector('#dog-image-container')
+    const imageWrapper = document.createElement('div')
+
+    imageWrapper.innerHTML = `<img src="${image}">`
+    container.appendChild(imageWrapper)
+}
+
+  function renderImages (images) {
+    images.forEach(image => renderImage(image) )
+  }
+  
+getImages()
+    .then((images) => renderImages(images))
+
